643588acb709e32ad7fd2fa23a534b3eade657c0
Chuck <chuck.franco@hotmail.com>
2023-11-21 20:51:35 -0600

    initial project setup

---
+++ breathr/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
