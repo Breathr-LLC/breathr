63570f6fdb87eb35d7598e6ccfb3c61ab6857dc3
Chuck <chuck.franco@hotmail.com>
2023-11-22 20:16:50 -0600

    initial routes defined

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Poems from './components/Poems';
+import ColoringBook from './components/ColoringBook';
+import Journal from './components/Journal';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello!</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/poems' element={<Poems/>} />
+        <Route path='/coloring-book' element={<ColoringBook/>} />
+        <Route path='/Journdal' element={<Journal/>} />
+        <Route path='/Login' element={<Login/>} />
+        <Route path='/Signup' element={<Signup/>} />
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
