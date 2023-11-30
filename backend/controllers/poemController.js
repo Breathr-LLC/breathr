bd6d427c1717397d14410975e238c4f48003f142
j9peters <29951738+j9peters@users.noreply.github.com>
2023-11-29 17:42:30 -0800

    updated poem routes to comply with HTTP standards

--- backend/controllers/poemController.js
+++ backend/controllers/poemController.js
@@ -35,15 +35,7 @@ poemController.markFavorite = async (req, res, next) => {
 
 // remove a poem from the given user's favorites
 poemController.removeFavorite = async (req, res, next) => {
-  const { userID, poemID } = req.body;
-  // verify that request supplies both userID and poemID
-  if (!userID || !poemID) {
-    return next(createErr({
-      method: 'removeFavorite',
-      type: 'Parsing request body',
-      err: 'User ID and/or poem ID not provided'
-    }));
-  }
+  const { userID, poemID } = req.params;
   const dbResponse = await poemsModel.removeFavorite(userID, poemID);
   // check whether error occurred while accessing db
   if (dbResponse.code) {
@@ -59,15 +51,7 @@ poemController.removeFavorite = async (req, res, next) => {
 // get favorite poems data (poem_id, title, author) for the given user
 // allows for creation of list of favorite poems on frontend
 poemController.getFavorites = async (req, res, next) => {
-  const { userID } = req.body;
-  // verify that request supplies userID
-  if (!userID) {
-    return next(createErr({
-      method: 'getFavorites',
-      type: 'Parsing request body',
-      err: 'User ID not provided'
-    }));
-  }
+  const { userID } = req.params;
   const dbResponse = await poemsModel.getFavorites(userID);
   // check whether error occurred while accessing db
   if (dbResponse.code) {
@@ -98,15 +82,7 @@ poemController.getAllPoems = async (req, res, next) => {
 };
 
 poemController.getSinglePoem = async (req, res, next) => {
-  const { poemID } = req.body;
-  // verify that request supplies poemID
-  if (!poemID) {
-    return next(createErr({
-      method: 'getSinglePoem',
-      type: 'Parsing request body',
-      err: 'Poem ID not provided'
-    }));
-  }
+  const { poemID } = req.params;
   const dbResponse = await poemsModel.getSinglePoem(poemID);
   // check whether error occurred while accessing db
   if (dbResponse.code) {
--- backend/routes/poems.js
+++ backend/routes/poems.js
@@ -3,9 +3,9 @@ const express = require('express');
 const poemRouter = express.Router();
 
 poemRouter.post('/markFavorite', poemController.markFavorite, (req, res) => res.sendStatus(201));
-poemRouter.delete('/removeFavorite', poemController.removeFavorite, (req, res) => res.sendStatus(204));
-poemRouter.get('/getFavorites', poemController.getFavorites, (req, res) => res.status(200).json(res.locals.favorites));
+poemRouter.delete('/removeFavorite/user-:userID(\\d+)/poem-:poemID(\\d+)', poemController.removeFavorite, (req, res) => res.sendStatus(204));
+poemRouter.get('/getFavorites/user-:userID(\\d+)', poemController.getFavorites, (req, res) => res.status(200).json(res.locals.favorites));
 poemRouter.get('/getAllPoems', poemController.getAllPoems, (req, res) => res.status(200).json(res.locals.poems));
-poemRouter.get('/getSinglePoem', poemController.getSinglePoem, (req, res) => res.status(200).json(res.locals.poem));
+poemRouter.get('/getSinglePoem/poem-:poemID(\\d+)', poemController.getSinglePoem, (req, res) => res.status(200).json(res.locals.poem));
 
 module.exports = poemRouter;
