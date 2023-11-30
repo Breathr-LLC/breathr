39fb3f016231f8efd48f0fe52d9f75e36464f22a
Chuck <chuck.franco@hotmail.com>
2023-11-29 19:16:40 -0600

    postcss.config.js configured

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 const Sidebar = () => {
   return (
-    <aside className="w-64 h-screen bg-surfaceVariant-light text-onSurfaceVariant-light p-4 fixed">
+    <aside className="w-48 h-full bg-surfaceVariant-light text-onSurfaceVariant-light p-4 fixed font-red-hat-display border-r-2 border-outline-light border-opacity-50">
       <ul className="flex flex-col space-y-4">
-        <a className="hover:bg-gray-700 p-2 rounded cursor-pointer" href="/logout">Logout</a>
-        <a className="hover:bg-gray-700 p-2 rounded cursor-pointer" href="/poems">Poems</a>
-        <a className="hover:bg-gray-700 p-2 rounded cursor-pointer" href="journal">Journal</a>
-        <a className="hover:bg-gray-700 p-2 rounded cursor-pointer" href="coloring-book">Coloring Book</a>
+        <a className="hover:bg-surface-light p-2 hover:text-onSurface-light p-2 rounded cursor-pointer" href="/poems">Poems</a>
+        <a className="hover:bg-surface-light p-2 hover:text-onSurface-light rounded cursor-pointer" href="journal">Journal</a>
+        <a className="hover:bg-surface-light p-2 hover:text-onSurface-light rounded cursor-pointer" href="coloring-book">Coloring Book</a>
       </ul>
     </aside>
   );
