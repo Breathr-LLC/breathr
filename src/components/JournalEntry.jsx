73b0eb24dae24401babdcdae9d6dcd54339f1356
Chuck <chuck.franco@hotmail.com>
2023-12-02 15:15:33 -0600

    styling added to journal

--- src/components/JournalEntry.jsx
+++ src/components/JournalEntry.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import DatePicker from './DatePicker';
 import dayjs from 'dayjs';
 import Picker from "emoji-picker-react";
@@ -8,6 +8,8 @@ export default function JournalEntry(props) {
     // access data passed in from Link component
     const { state } = useLocation();
     const [chosenEmoji, setChosenEmoji] = useState(null);
+
+    const navigate = useNavigate();
 
     const onEmojiClick = (event, emojiObject) => {
         setChosenEmoji(emojiObject);
@@ -31,48 +33,61 @@ export default function JournalEntry(props) {
                 date: currDate,
                 user_id: userID
             })
-        })
+          })
+          .then(
+            navigate("/journal")
+          )
         .catch(err => {
             console.log(err)
-        })
+        });
+      
     };
 
     return (
-        <div>
-            <form action="" id="entry">
-                <div className="entry--header">
-                    <DatePicker name="date" id="date" value={dayjs(currDate)}/>
-                    <div>
-                        {chosenEmoji ? (
-                            <span>
-                                Your Mood:
-                                <img
-                                    style={{ width: "30px" }}
-                                    src={chosenEmoji.target.src}
-                                    alt="Mood emoji"
-                                    name="category"
-                                    id="category"
-                                />
-                            </span>
-                        ) : (
-                            <span>What's your mood?</span>
-                            )}
-                        {!chosenEmoji && <Picker onEmojiClick={onEmojiClick} category={['smileys_people']} />}
-                    </div>
-                </div>
-                <main className="entry--main">
-                    <input type="text" placeholder="Title" className="entry-title" name="title" id="title"/>
-                    {/* <button>add photo(s)</button> */}
-                    <textarea 
-                        name="text" 
-                        id="text" 
-                        cols="30" 
-                        rows="10"
-                        className="entry-body"
-                    />
-                    <button type="submit" onClick={createNewEntry}>submit</button>
-                </main>
-            </form>
-        </div>
+       // JournalEntry component
+<div className="entry p-4">
+  <form action="" id="entry">
+    <div className="entry--header flex justify-between items-center mb-4">
+      <DatePicker name="date" id="date" value={dayjs(currDate)}/>
+      <div className="emoji-picker">
+        {chosenEmoji ? (
+          <span className="flex items-center">
+            Your Mood:
+            <img
+              className="w-8 h-8"
+              src={chosenEmoji.target.src}
+              alt="Mood emoji"
+              name="category"
+              id="category"
+            />
+          </span>
+        ) : (
+          <span>What's your mood?</span>
+        )}
+        {!chosenEmoji && <Picker onEmojiClick={onEmojiClick} category={['smileys_people']} />}
+      </div>
+    </div>
+    <main className="entry--main space-y-4">
+      <input
+        type="text"
+        placeholder="Title"
+        className="entry-title form-input block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+        name="title"
+        id="title"
+      />
+      <textarea
+        name="text"
+        id="text"
+        cols="30"
+        rows="10"
+        className="entry-body form-textarea mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+      />
+      <button type="button" onClick={createNewEntry} className="bg-primary-light hover:bg-primaryContainer-light hover:text-onPrimaryContainer-light text-white font-bold py-2 px-4 rounded">
+        submit
+      </button>
+    </main>
+  </form>
+</div>
+
     );
 }
