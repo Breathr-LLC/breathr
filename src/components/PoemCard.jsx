44323e1ce0be1fcc336f28378ae6edcc35071497
Chuck <chuck.franco@hotmail.com>
2023-12-02 12:49:22 -0600

    positioning of poem carousel updated

--- src/components/PoemCard.jsx
+++ src/components/PoemCard.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
-const Card = ({title, content}) => {
+const PoemCard = ({ title, author, poemID, handleFindSinglePoem, isExpanded, expandedPoemText, handleFavoriteToggle, isFavorited }) => {
+  const handleHeartClick = (e) => {
+    e.stopPropagation(); // Prevent the card click event
+    handleFavoriteToggle(poemID);
+  };
+
   return (
-
-  <div className='card'>
-    <h2>{title}</h2>
-    <p>{content}</p>
-  </div>
-  )
+    <div className={`card ${isExpanded ? 'expanded' : ''}`} onClick={() => handleFindSinglePoem(poemID)}>
+      <div className='heart-icon' onClick={handleHeartClick}>
+      </div>
+      <h2 className='title'>{title}</h2>
+      <div className='poem-card-details'>
+      <p className='author'>{author}</p>
+      <div>{isFavorited ? <FaHeart /> : <FaRegHeart />}</div>
+      </div>
+      {isExpanded && <p className="poem-text">{expandedPoemText}</p>}
+    </div>
+  );
 };
 
-export default Card;
+export default PoemCard;
